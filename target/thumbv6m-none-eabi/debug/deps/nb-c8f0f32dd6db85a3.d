d200c45a9f98ea4a09c82c305afb4b1bd66873e6
Alexander Berntsson <alexande.berntsson@gmail.com>
2023-08-18 14:05:26 +0200

    ignore target

--- target/thumbv6m-none-eabi/debug/deps/nb-c8f0f32dd6db85a3.d
+++
@@ -1,5 +0,0 @@
-/home/alexander/prog/magnetometer-playground/mlx90393/target/thumbv6m-none-eabi/debug/deps/nb-c8f0f32dd6db85a3.rmeta: /home/alexander/.cargo/registry/src/index.crates.io-6f17d22bba15001f/nb-0.1.3/src/lib.rs
-
-/home/alexander/prog/magnetometer-playground/mlx90393/target/thumbv6m-none-eabi/debug/deps/nb-c8f0f32dd6db85a3.d: /home/alexander/.cargo/registry/src/index.crates.io-6f17d22bba15001f/nb-0.1.3/src/lib.rs
-
-/home/alexander/.cargo/registry/src/index.crates.io-6f17d22bba15001f/nb-0.1.3/src/lib.rs:
