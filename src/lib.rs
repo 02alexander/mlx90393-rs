0147815d166d37416757f0617286e359ec703151
Alexander Berntsson <alexande.berntsson@gmail.com>
2023-08-18 19:23:42 +0200

    support for SPI

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@
 
 */
 
-use embedded_hal::blocking::delay::DelayMs;
+use embedded_hal::{blocking::delay::DelayMs, digital::v2::OutputPin};
 
 #[derive(Clone, Copy, Debug)]
 #[repr(u8)]
@@ -36,17 +36,6 @@ pub enum Command {
 pub enum Error {
     IO,
     Sensor(u8),
-}
-
-pub struct Magnetometer<'a, D> {
-    address: u8,
-    i2c: &'a mut D,
-    gain: Gain,
-    resolution_x: Resolution,
-    resolution_y: Resolution,
-    resolution_z: Resolution,
-    digital_filter: DigitalFilter,
-    oversampling_ratio: OverSamplingRatio,
 }
 
 /// Available configurations of the gain.
@@ -111,14 +100,73 @@ const TCONV: [[f32; 4]; 8] = [
     [25.65, 50.61, 100.53, 200.37],
 ];
 
-impl<'a, D> Magnetometer<'a, D>
+/// Trait required for communication with the sensor.
+pub trait Tranceive {
+    fn tranceive(&mut self, send: &[u8], recv: &mut [u8]) -> Result<(), Error>;
+}
+
+pub struct I2CInterface<'a, D> {
+    pub i2c: &'a mut D,
+    pub address: u8,
+}
+pub struct SPIInterface<'a, D, C> {
+    pub spi: &'a mut D,
+    pub cs: C,
+}
+
+pub struct Magnetometer<P> {
+    protocol: P,
+    gain: Gain,
+    resolution_x: Resolution,
+    resolution_y: Resolution,
+    resolution_z: Resolution,
+    digital_filter: DigitalFilter,
+    oversampling_ratio: OverSamplingRatio,
+}
+
+impl<'a, D> Tranceive for I2CInterface<'a, D>
 where
     D: embedded_hal::blocking::i2c::WriteRead,
 {
-    pub fn new(address: u8, i2c: &'a mut D) -> Magnetometer<'a, D> {
+    fn tranceive(&mut self, send: &[u8], recv: &mut [u8]) -> Result<(), Error> {
+        self.i2c
+            .write_read(self.address, send, recv)
+            .map_err(|_| Error::IO)
+    }
+}
+
+impl<'a, D, C> Tranceive for SPIInterface<'a, D, C>
+where
+    D: embedded_hal::blocking::spi::Transfer<u8>,
+    C: OutputPin,
+{
+    fn tranceive(&mut self, send: &[u8], recv: &mut [u8]) -> Result<(), Error> {
+        self.cs.set_low().map_err(|_| Error::IO)?;
+        let mut buf = [0; 16]; // The same buffer is used for sending and transmitting data.
+        if buf.len() < send.len() {
+            return Err(Error::IO);
+        }
+        buf[..send.len()].copy_from_slice(send);
+        let received = self
+            .spi
+            .transfer(&mut buf[..(send.len() + recv.len())])
+            .map_err(|_| Error::IO)?;
+        if recv.len() != received.len() - send.len() {
+            return Err(Error::IO);
+        }
+        recv.copy_from_slice(&received[send.len()..]);
+        self.cs.set_high().map_err(|_| Error::IO)?;
+        Ok(())
+    }
+}
+
+impl<P> Magnetometer<P>
+where
+    P: Tranceive,
+{
+    pub fn new(protocol: P) -> Magnetometer<P> {
         Magnetometer {
-            address,
-            i2c,
+            protocol,
             gain: Gain::X5,
             resolution_x: Resolution::RES1,
             resolution_y: Resolution::RES1,
@@ -153,7 +201,7 @@ where
             Axis::Y => 7,
             Axis::Z => 9,
         };
-        let mask = !((0b11 as u16) << offset);
+        let mask = !((0b11_u16) << offset);
         let new_reg = (reg & mask) | ((resolution as u16) << offset);
         self.memory_write(0x02, new_reg)?;
         Ok(())
@@ -162,7 +210,7 @@ where
     pub fn set_gain(&mut self, gain: Gain) -> Result<(), Error> {
         let reg = self.memory_read(0x02)?;
         let offset = 4;
-        let mask = !((0b111 as u16) << offset);
+        let mask = !((0b111_u16) << offset);
         let new_reg = (reg & mask) | ((gain as u16) << offset);
         self.memory_write(0x00, new_reg)?;
         self.gain = gain;
@@ -173,7 +221,7 @@ where
     pub fn set_filter(&mut self, filter: DigitalFilter) -> Result<(), Error> {
         let reg = self.memory_read(0x02)?;
         let offset = 2;
-        let mask = !((0b1111 as u16) << offset);
+        let mask = !((0b1111_u16) << offset);
         let new_reg = (reg & mask) | ((filter as u16) << offset);
         self.memory_write(0x02, new_reg)?;
         self.digital_filter = filter;
@@ -184,7 +232,7 @@ where
     pub fn set_oversampling_ratio(&mut self, ratio: OverSamplingRatio) -> Result<(), Error> {
         let reg = self.memory_read(0x02)?;
         let offset = 0;
-        let mask = !((0b11 as u16) << offset);
+        let mask = !((0b11_u16) << offset);
         let new_reg = (reg & mask) | ((ratio as u16) << offset);
         self.memory_write(0x02, new_reg)?;
         self.oversampling_ratio = ratio;
@@ -193,10 +241,10 @@ where
 
     /// Starts a measurement.
     pub fn start_measurement(&mut self) -> Result<(), Error> {
-        let mut buffer = [0 as u8; 1];
+        let mut buffer = [0; 1];
         let cmd_byte = Command::SM as u8 | 0xf;
-        self.i2c
-            .write_read(self.address, &[cmd_byte], &mut buffer)
+        self.protocol
+            .tranceive(&[cmd_byte], &mut buffer)
             .map_err(|_| Error::IO)?;
 
         status_err(buffer[0])?;
@@ -205,10 +253,10 @@ where
 
     /// Collects the data from a measurement. `start_measurment` must be called before to start one.
     pub fn collect_measurement(&mut self) -> Result<(i16, i16, i16, i16), Error> {
-        let mut buffer = [0 as u8; 9];
+        let mut buffer = [0; 9];
         let cmd_byte = Command::RM as u8 | 0xf;
-        self.i2c
-            .write_read(self.address, &[cmd_byte], &mut buffer)
+        self.protocol
+            .tranceive(&[cmd_byte], &mut buffer)
             .map_err(|_| Error::IO)?;
         status_err(buffer[0])?;
         let t = i16::from_be_bytes([buffer[1], buffer[2]]);
@@ -241,10 +289,10 @@ where
     /// NOTE: The non-volatime memory wasn't meant to be written to a lot
     /// so the number of write-cycles should be kept to a minimum.
     pub fn store(&mut self) -> Result<(), Error> {
-        let mut buffer = [0 as u8; 1];
+        let mut buffer = [0; 1];
         let cmd_byte = Command::HS as u8;
-        self.i2c
-            .write_read(self.address, &[cmd_byte], &mut buffer)
+        self.protocol
+            .tranceive(&[cmd_byte], &mut buffer)
             .map_err(|_| Error::IO)?;
         status_err(buffer[0])?;
         Ok(())
@@ -254,9 +302,9 @@ where
     /// volatile memory.
     pub fn recall(&mut self) -> Result<(), Error> {
         let cmd_byte = Command::HR as u8;
-        let mut buffer = [0 as u8; 1];
-        self.i2c
-            .write_read(self.address, &[cmd_byte], &mut buffer)
+        let mut buffer = [0; 1];
+        self.protocol
+            .tranceive(&[cmd_byte], &mut buffer)
             .map_err(|_| Error::IO)?;
         status_err(buffer[0])?;
         Ok(())
@@ -265,9 +313,9 @@ where
     /// Cancels the current measurement.
     pub fn exit(&mut self) -> Result<(), Error> {
         let cmd_byte = Command::EX as u8;
-        let mut buffer = [0 as u8; 1];
-        self.i2c
-            .write_read(self.address, &[cmd_byte], &mut buffer)
+        let mut buffer = [0; 1];
+        self.protocol
+            .tranceive(&[cmd_byte], &mut buffer)
             .map_err(|_| Error::IO)?;
         status_err(buffer[0])?;
         Ok(())
@@ -276,9 +324,9 @@ where
     /// Resets the device.
     pub fn reset(&mut self) -> Result<(), Error> {
         let cmd_byte = Command::RT as u8;
-        let mut buffer = [0 as u8; 1];
-        self.i2c
-            .write_read(self.address, &[cmd_byte], &mut buffer)
+        let mut buffer = [0; 1];
+        self.protocol
+            .tranceive(&[cmd_byte], &mut buffer)
             .map_err(|_| Error::IO)?;
         status_err(buffer[0])?;
         Ok(())
@@ -290,10 +338,9 @@ where
     pub fn memory_write(&mut self, mem_adress: u8, data: u16) -> Result<(), Error> {
         let cmd_byte = Command::WR as u8;
         let shifted_address = mem_adress << 2;
-        let mut buffer = [0 as u8; 1];
-        self.i2c
-            .write_read(
-                self.address,
+        let mut buffer = [0; 1];
+        self.protocol
+            .tranceive(
                 &[
                     cmd_byte,
                     (data >> 8) as u8,
@@ -314,9 +361,9 @@ where
     pub fn memory_read(&mut self, mem_adress: u8) -> Result<u16, Error> {
         let cmd_byte = Command::RR as u8;
         let shifted_address = mem_adress << 2;
-        let mut buffer = [0 as u8; 3];
-        self.i2c
-            .write_read(self.address, &[cmd_byte, shifted_address], &mut buffer)
+        let mut buffer = [0; 3];
+        self.protocol
+            .tranceive(&[cmd_byte, shifted_address], &mut buffer)
             .map_err(|_| Error::IO)?;
         status_err(buffer[0])?;
         Ok(u16::from_be_bytes([buffer[1], buffer[2]]))
